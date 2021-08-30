4acef652875fc030dbd4ca3514403c9360f45760
Robert Gallagher <rgallagher496@gmail.com>
2021-08-30 17:10:25 -0400

    various python programs used to practice concepts in Mod 3

---
+++ grade_practice.py
@@ -0,0 +1,12 @@
+score = int(input("What is your test score?"))
+
+if score >= 90:
+    print("Your grade is an A.")
+elif score >= 80:
+    print('Your grade isa B.')
+elif score >= 70:
+    print('Your grade is a C.')
+elif score >= 60:
+    print('Your Grade is a D.')
+else:
+    print('Your grade is an F.')
